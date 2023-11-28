ff9786cfaf5380ece18d2087fb41cad95e0f9e39
Sarah Morgen <sarah.morgen@outlook.de>
2023-11-28 12:35:47 +0100

    Solution

--- js/index.js
+++ js/index.js
@@ -2,6 +2,11 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {  // musste meine Function async schreiben
+    const response = await fetch (url); //sonst funktioniert await nicht
+    const data = await response.json()  //da fetch asyncron ist, müssen wir awaiten
+                                        //warte auf die antwort des Servers. Json ist ein Datenformat wie Daten übertragen werden!
+    console.log(data);
+}
 
-fetchData();
+await fetchData();  //wäre auch ohne gegangen.
